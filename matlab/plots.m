fc575a32023d97bb52a47640767061c0acb3f817
despresj <despresj@mail.gvsu.edu>
2021-09-17 10:13:40 -0400

    including plots

---
+++ matlab/plots.m
@@ -0,0 +1,4 @@
+rooms = 1000;
+bookings = 1500;
+p = 0.64;
+plot(binopdf(1:rooms, bookings, p))
---
+++ matlab/regression.m
@@ -0,0 +1,5 @@
+file = "/Users/josephdespres/Documents/MSU/MTH 843 Industrial Mathematics/Industrial-Mathematics/data/hotel.csv"
+hotel = readmatrix(file);
+hotel_table = readtable(file);
+
+hotel_table
