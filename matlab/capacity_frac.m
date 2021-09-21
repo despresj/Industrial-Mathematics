4df97b14a7998e5878793af9998eb43ac6ea5b3b
despresj <despresj@mail.gvsu.edu>
2021-09-21 14:51:08 -0400

    worked on function

--- matlab/capacity_frac.m
+++ matlab/capacity_frac.m
@@ -4,12 +4,17 @@ function frac_of_capacity = capacity_frac(p, rooms, loss)
 
     while E_m1 >= E
         E = bookings * Fx - loss * bookings * P_overbook;
-        
+  
         Fx = binocdf(rooms, bookings, p);
         P_overbook = 1 - Fx;
-        E_m1 = bookings * Fx - loss * bookings * P_overbook;
-
         bookings = bookings + 1;
+        
+        if bookings > 2e3
+            bookings = bookings + 1e3;
+            break
+        end
+        
+        E_m1 = bookings * Fx - loss * bookings * P_overbook;
     end
     frac_of_capacity = bookings;
 end
--- matlab/regression.m
+++ matlab/regression.m
@@ -6,13 +6,14 @@ y = hotel(:, 2);
 y = categorical(y);
 [B,dev,stats] = mnrfit(X, y);
 p_q = mnrval(B, X);
-phat_vec = p_q(:, 1);
+
+phat_vec = p_q(1:1000, 1);
 
 capacity_vec = zeros(1, length(phat_vec));
 
 for i = 1:length(phat_vec)
-    book = capacity_frac(phat_vec(i), 1000, 4)
+    book = capacity_frac(phat_vec(i), 1000, 4);
    capacity_vec(i) = book;
 end 
-
+beep
 capacity_vec
