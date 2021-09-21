26aa1a98098e1101a1b6132457a1814d358d95f9
despresj <despresj@mail.gvsu.edu>
2021-09-21 15:29:21 -0400

    adding functions

--- matlab/capacity_frac.m
+++ matlab/capacity_frac.m
@@ -5,15 +5,21 @@ function frac_of_capacity = capacity_frac(p, rooms, loss)
     filled_m1 = 0;
     over_m1 = 0;
     bookings = rooms;
+
     while E_m1 >= E
         
+        if bookings > 1e4
+            break
+        end
+
         E = sum(filled_m1) - loss * sum(over_m1);
+        
+        bookings = bookings + 1;
+        
         filled_m1 = bookings * binopdf(1:rooms, bookings, p);
         over_m1 = bookings * binopdf(rooms+1:bookings, bookings, p);
         E_m1 = sum(filled_m1) - loss * sum(over_m1);
-
-        bookings = bookings + 1;
-
+        
     end
     frac_of_capacity = bookings;
 end
--- matlab/plots.m
+++ matlab/plots.m
@@ -1,4 +1,15 @@
 rooms = 1000;
 bookings = 1500;
 p = 0.64;
-plot(binopdf(1:rooms, bookings, p))
+% plot(binopdf(1:rooms, bookings, p))
+% plot(binopdf(rooms+1, bookings, p))
+
+p_vec = 0:0.01:1;
+
+y = zeros(1, length(p_vec));
+
+for i = 1:length(p_vec)
+    y(i) = capacity_frac(p_vec(i), 1000, 1500)
+end
+
+plot(p_vec, y)
