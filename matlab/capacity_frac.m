cc7fdca470a14ae9e1160c7f633cb8ac8dfe223b
despresj <despresj@mail.gvsu.edu>
2021-09-27 09:02:45 -0400

    adding faster function

--- matlab/capacity_frac.m
+++ matlab/capacity_frac.m
@@ -1,20 +1,18 @@
 function frac_of_capacity = capacity_frac(p, rooms, loss)
 
     [E, E_m1, Fx, P_overbook, bookings] = deal(0, 0, 0, 0, rooms);
+    if p > 0.3
+        while E_m1 >= E
+            E = bookings * Fx - loss * bookings * P_overbook;
 
-    while E_m1 >= E
-        E = bookings * Fx - loss * bookings * P_overbook;
-  
-        Fx = binocdf(rooms, bookings, p);
-        P_overbook = 1 - Fx;
-        bookings = bookings + 1;
-        
-        if bookings > 2e3
-            bookings = bookings + 1e3;
-            break
+            Fx = binocdf(rooms, bookings, p);
+            P_overbook = 1 - Fx;
+            bookings = bookings + 1;
+
+            E_m1 = bookings * Fx - loss * bookings * P_overbook;
         end
-        
-        E_m1 = bookings * Fx - loss * bookings * P_overbook;
+    else
+        bookings = bookings + 2e3;
     end
     frac_of_capacity = bookings;
 end
--- matlab/plots.m
+++ matlab/plots.m
@@ -3,14 +3,12 @@ bookings = 1500;
 p = 0.64;
 % plot(binopdf(1:rooms, bookings, p))
 
-
-
 p_vec = 0:0.01:1;
 
 y = zeros(1, length(p_vec));
 
 for i = 1:length(p_vec)
-    y(i) = capacity_frac(p_vec(i), 1000, 1500)
+   y(i) = capacity_frac(p_vec(i), 1000, 4);
 end
 
 plot(p_vec, y)
