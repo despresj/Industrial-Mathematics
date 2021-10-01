d11354d6cdbf51fd44a200301b85d1582bbd38b2
despresj <despresj@mail.gvsu.edu>
2021-09-30 20:24:06 -0400

    updated report

---
+++ report/plots/flowchart.R
@@ -0,0 +1,13 @@
+DiagrammeR::grViz("digraph {
+  graph [layout = dot, rankdir = TB]
+  
+  node [shape = rectangle]        
+  rec1 [label = '1. Specify hotel capacity and cost of overbooking']
+  rec2 [label = '2. Estimate probability of a guest arriving given relevant factors']
+  rec3 [label =  '3. Using the estimated probability estimate the fraction of hotel capacity']
+  rec4 [label = '4. Valadate the method using resampling']
+  
+  # edge definitions with the node IDs
+  rec1 -> rec2 -> rec3 -> rec4
+  }",
+  height = 500)
